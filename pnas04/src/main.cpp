001e2b96786c3f1782cf5e13d15df632be3d2eba
myfavouritekk <myfavouritekk@sina.com>
2012-07-12 11:34:44 +0800

    adding group evolution feature

--- pnas04/src/main.cpp
+++ pnas04/src/main.cpp
@@ -6,20 +6,33 @@ int main (void) {
 
     srand((unsigned int)time(NULL));
     //  initialization
-    Population myPop;
-    myPop.init ();
+    // initialize several Populations and run simultaneously
+    
+    Population** myPops = new Population*[5];
+    for (int i = 0; i < 5; i++) {
+        myPops[i] = new Population(20);
+        myPops[i] -> init();
+    }
+    
 
     //  evolution
-    while (!myPop.isTerminate ()) {
-        myPop.growth ();
-        myPop.selection ();
+    while (!myPops[0] -> isTerminate ()) {
+        for (int i = 0 ; i < 5; i++) {
+            myPops[i] -> growth ();
+            myPops[i] -> selection ();
+
+        }
     }
 
-    myPop.classification();
+    //myPops.classification();
     
     //  output
-    myPop.output();
-    myPop.genXMLFormat();
+    
+    for (int i = 0; i < 5; i++) {
+        myPops[i] -> output();
+
+    }
+    //myPops.genXMLFormat();
 
     return 0;
 }

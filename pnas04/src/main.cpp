cb8d72cba3fa3064479fdfa9d6641f3bff968bbb
myfavouritekk <myfavouritekk@sina.com>
2012-07-17 10:32:14 +0800

    changed main method to mutate for topologies first and then for parameters

--- pnas04/src/main.cpp
+++ pnas04/src/main.cpp
@@ -1,33 +1,32 @@
 #include "population.h"
 #include <ctime>
-
+#define TOTAL_EVO 1000
 
 int main (void) {
 
     srand((unsigned int)time(NULL));
+    
+    int evolutionForTopology = 10;
+    
     //  initialization
-    Population myPop(100);
+    Population myPop(20, TOTAL_EVO);
     myPop.init ();
 
     //  evolution
     while (!myPop.isTerminate ()) {
-        if (myPop.getEvolution() % 200 == 0) {
-            //sort
-            myPop.sort();
-            //  output
-            myPop.output();
+        
+        if (myPop.getEvolution() <= evolutionForTopology) {
             myPop.mutation();
+        }else{
+            myPop.mut_parameters();
         }
-        myPop.mut_parameters();
-        std::cout << "Finished Evolution: " << 999 - myPop.getEvolution() << std::endl;
+        std::cout << "Finished Evolution: " << TOTAL_EVO - myPop.getEvolution() << std::endl;
     }
 
-    //myPop.classification();
     
     //  output
     myPop.sort();
     myPop.output();
-    //myPop.genXMLFormat();
 
     return 0;
 }

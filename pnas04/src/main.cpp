e57eaa4a8f4f848fb274328ca9b50bc3071d0bd9
myfavouritekk <myfavouritekk@sina.com>
2012-07-17 13:22:05 +0800

    revert to previous evolution scheme where parameter mutations are in between topology mutations

--- pnas04/src/main.cpp
+++ pnas04/src/main.cpp
@@ -5,22 +5,20 @@
 int main (void) {
 
     srand((unsigned int)time(NULL));
-    
-    int evolutionForTopology = 10;
-    
     //  initialization
-    Population myPop(20, TOTAL_EVO);
+    Population myPop(100, TOTAL_EVO);
     myPop.init ();
 
     //  evolution
     while (!myPop.isTerminate ()) {
-        
-        if (myPop.getEvolution() <= evolutionForTopology) {
+        if (myPop.getEvolution() % 200 == 0) {
+            //sort
+            myPop.sort();
+            myPop.output();
             myPop.mutation();
-        }else{
-            myPop.mut_parameters();
         }
-        std::cout << "Finished Evolution: " << TOTAL_EVO - myPop.getEvolution() << std::endl;
+        myPop.mut_parameters();
+        std::cout << "Finished Evolution: " << 999 - myPop.getEvolution() << std::endl;
     }
 
     

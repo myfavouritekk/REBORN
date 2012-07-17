f868e7abce2159bf206fed6453832de885580faa
myfavouritekk <myfavouritekk@sina.com>
2012-07-17 15:37:04 +0800

    adjust generations of revolutions so that the more complex the topology, the more generations needed to change parameters

--- pnas04/src/main.cpp
+++ pnas04/src/main.cpp
@@ -1,21 +1,25 @@
 #include "population.h"
 #include <ctime>
-#define TOTAL_EVO 1000
+#define TOTAL_EVO 499
 
 int main (void) {
 
     srand((unsigned int)time(NULL));
     //  initialization
-    Population myPop(100, TOTAL_EVO);
+    Population myPop(200, TOTAL_EVO);
     myPop.init ();
 
+    int i = 1;
+    int sum = 1;
     //  evolution
     while (!myPop.isTerminate ()) {
-        if (myPop.getEvolution() % 200 == 0) {
+        if ((TOTAL_EVO + 1 - myPop.getEvolution()) % (50 * sum) == 0) {
             //sort
             myPop.sort();
             myPop.output();
             myPop.mutation();
+            i++;
+            sum += i;
         }
         myPop.mut_parameters();
         std::cout << "Finished Evolution: " << TOTAL_EVO - myPop.getEvolution() << std::endl;

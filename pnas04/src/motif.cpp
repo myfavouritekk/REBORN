cf654a188068bf5724035bf8e21f9988f4638341
myfavouritekk <myfavouritekk@sina.com>
2012-07-30 15:32:58 +0800

    adding a new class called Motif: this class is for representing the basic motifs in a cell, it contains a vector storing nodes and a 2-D matrix storing the regulatory relationship

---
+++ pnas04/src/motif.cpp
@@ -0,0 +1,34 @@
+//
+//  motif.cpp
+//  PNAS04Team
+//
+//  Created by Jack Kang on 12-7-30.
+//
+//
+
+#include "motif.h"
+
+namespace ustc {
+    
+Motif::Motif(std::vector<Node*>* constructingNodes, int** relationshipMatrix){
+    
+}
+
+Motif::~Motif(){
+    
+}
+
+
+int Motif::getMtype(){
+    return mtype;
+}
+
+
+//private method to generate motif type based on the motifMatrix
+void Motif::generateMtype(){
+    
+}
+
+    
+    
+}   //namespace ustc
---
+++ pnas04/src/motif.h
@@ -0,0 +1,47 @@
+//
+//  motif.h
+//  PNAS04Team
+//
+//  Created by Jack Kang on 12-7-30.
+//
+//
+
+#ifndef __PNAS04Team__motif__
+#define __PNAS04Team__motif__
+
+#include <iostream>
+#include "consts.h"
+#include "node.h"
+
+namespace ustc {
+    
+class Motif{
+    public:
+        //constructor: no default constructor
+        Motif(std::vector<Node*>* constructingNodes, int** relationshipMatrix);
+        ~Motif();
+        
+        //return the motif type
+        int getMtype();
+        
+        
+    private:
+        //private method to generate motif type based on the motifMatrix
+        void generateMtype();
+    
+        //containing the pointers of the constructing nodes of the motif
+        std::vector<Node*> motifNodes;
+    
+        //storing the regulatory relationships in the motif
+        int** motifMatrix;
+    
+        //motif type
+        int mtype;
+    
+};  //class Motif definition
+    
+    
+}   // namespace ustc
+
+
+#endif /* defined(__PNAS04Team__motif__) */

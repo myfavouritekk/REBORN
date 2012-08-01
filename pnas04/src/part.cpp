70d72a14b9c22c6b01357ce919bc5c4332a3669e
Francis Chen <kfrancischen@gmail.com>
2012-08-01 15:57:31 +0800

    finished functions in class Part

--- pnas04/src/part.cpp
+++ pnas04/src/part.cpp
@@ -10,9 +10,23 @@
 
 
 namespace ustc {
+Part::Part(const part_Type& _pType,const std::string& _id,const std::string& _name){
+	pType = _pType;
+	id = _id;
+	name = _name;
+}
     
-    
-    
+part_Type Part::getPtype(){
+	return pType;
+}
+
+std::string Part::getId(){
+	return id;
+}
+
+std::string Part::getName(){
+	return name;
+}
     
     
 }// namespace ustc

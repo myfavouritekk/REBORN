4a7cedbf70098020cee0018f2d3e65ac18218a04
myfavouritekk <myfavouritekk@gmail.com>
2012-08-08 12:49:57 +0800

    fixed reading gene names bug when reading openron data files

--- pnas04/src/operon.cpp
+++ pnas04/src/operon.cpp
@@ -30,18 +30,23 @@ Operon::Operon(const std::string _name){
     int* nameIndex = new int[numGene + 1];
     nameIndex[0] = 0;
     for (int i = 0; i < numGene; i++) {
-        nameIndex[i + 1] = (int)geneNames.find(",", nameIndex[i] + 1, (int)geneNames.size());
-        int start, end;
-        if (i == 0) 
+        nameIndex[i + 1] = (int)geneNames.find(",", nameIndex[i] + 1);
+        if (i == numGene - 1) {//   there is no last ",", nameIndex[i+1] is -1
+            nameIndex[i + 1] = (int)geneNames.size();   //  assign it to the end of the string
+        }
+        int start, length;
+        if (i == 0){
             start = 0;
-        else
+            length = nameIndex[i + 1] - nameIndex[i];
+        }
+        else{
             start = nameIndex[i] + 1;
+            length = nameIndex[i + 1] - nameIndex[i] - 1;
+        }
         if (i == numGene - 1)
-            end = (int)geneNames.size();
-        else
-            end = nameIndex[i + 1] - 1;
+            length = (int)geneNames.size(); //  certainly out of range and will read to the end of the string
             
-        std::string newGeneString = geneNames.substr(start, end);
+        std::string newGeneString = geneNames.substr(start, length);
         genes.push_back(newGeneString);
     }
     
